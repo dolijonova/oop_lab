6d3b5c77559b0c61f73e8ed5a932e34e88c18890
Zarnigor <z.dolijonova@newuu.uz>
2025-08-26 11:26:17 +0500

    Initial Commit

---
+++ oop_lab/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  oop_lab
+//
+//  Created by Zarnigor Dolijonova on 26/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    for(int i=0;i<5;i++){
+        std::cout<<i;
+    }
+    return 0;
+}
